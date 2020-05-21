774efe6bf877ad6fc441427209317f49f00022d0
Jakie W. Rice <jakie.rice@gmail.com>
2020-05-21 14:52:08 -0500

    styles: add basic polish to landing page

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-import { Typography } from "@material-ui/core";
+import { Grid, Container, Typography } from "@material-ui/core";
 
 function HomePage() {
   return (
-    <>
-      <Typography variant="h1">Welcome to Mittere!</Typography>
-      <Typography variant="body1">
-        Soon this page will have all kinds of wizz-bang tools to help you build
-        a network or supporters and raise money foryour missions!
-      </Typography>
-    </>
+    <Container>
+      <Grid container>
+        <Typography variant="h1">Welcome to Mittere!</Typography>
+        <Typography variant="body1">
+          Soon this page will have all kinds of wizz-bang tools to help you
+          build a network or supporters and raise money foryour missions!
+        </Typography>
+      </Grid>
+    </Container>
   );
 }
 
--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -4,60 +4,63 @@ import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/pipeable";
 import * as RemoteData from "@devexperts/remote-data-ts";
 
-import { Button, CircularProgress, Typography } from "@material-ui/core";
+import {
+  makeStyles,
+  Button,
+  Box,
+  Paper,
+  CircularProgress,
+  Typography
+} from "@material-ui/core";
 
 import { AuthContext, signIn } from "./Auth";
 
 const renderLoginMessage = () => (
-  <>
-    <Typography variant="h1">Mittere</Typography>
-    <Typography variant="body1">
-      Take control of your support pipeline so you can support the world.
-      <Button onClick={signIn}>Login</Button>
-    </Typography>
-  </>
+  <Paper elevation={4}>
+    <Box
+      padding={8}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant="h1" align="center">
+        Mittere
+      </Typography>
+      <Typography variant="subtitle1" align="center">
+        Take control of your support pipeline so you can support the world.
+      </Typography>
+      <Box m={2}>
+        <Button variant="contained" color="primary" onClick={signIn}>
+          Login
+        </Button>
+      </Box>
+    </Box>
+  </Paper>
 );
 
-function LoginPage() {
+function LandingPage() {
   const currentUser = useContext(AuthContext);
 
-  return pipe(
-    currentUser,
-    RemoteData.fold(
-      () => null,
-      () => (
-        <div
-          style={{
-            height: "100vh",
-            width: "100vw",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <CircularProgress />
-        </div>
-      ),
-      () => (
-        <div
-          style={{
-            height: "100vh",
-            width: "100vw",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          'There is an error with authentication.'
-        </div>
-      ),
-      user =>
-        pipe(
-          user,
-          O.fold(renderLoginMessage, () => <Redirect to="/home" />)
+  return (
+    <Box p={16}>
+      {pipe(
+        currentUser,
+        RemoteData.fold(
+          () => null,
+          () => <CircularProgress />,
+          () => (
+            <Typography>'There is an error with authentication.'</Typography>
+          ),
+          user =>
+            pipe(
+              user,
+              O.fold(renderLoginMessage, () => <Redirect to="/home" />)
+            )
         )
-    )
+      )}
+    </Box>
   );
 }
 
-export default LoginPage;
+export default LandingPage;
