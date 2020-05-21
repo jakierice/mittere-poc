585fe8300dc18922fd69ac7a7d50285f18c302a1
Jakie W. Rice <jakie.rice@gmail.com>
2020-05-21 12:25:56 -0500

    feat: move nav drawer and app header app layout to private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,197 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { Typography } from '@material-ui/core';
 
 import { AuthProvider } from "./Auth";
 import PrivateRoute from "./PrivateRoute";
-import Navigation from "./Navigation";
+import ContactsPage from "./ContactsPage";
 import LoginPage from "./LoginPage";
 
-import clsx from "clsx";
-import {
-  AppBar,
-  CssBaseline,
-  Divider,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-  makeStyles,
-  useTheme
-} from "@material-ui/core";
-import {
-  ChevronLeft as ChevronLeftIcon,
-  ChevronRight as ChevronRightIcon,
-  Contacts as ContactsIcon,
-  Menu as MenuIcon
-} from "@material-ui/icons";
-
-const DRAWER_WIDTH = 240;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${DRAWER_WIDTH}px)`,
-    marginLeft: DRAWER_WIDTH,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: DRAWER_WIDTH,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: DRAWER_WIDTH
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -DRAWER_WIDTH
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
-}));
-
-export default function AppLayout() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+export default function App() {
   return (
     <AuthProvider>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              Mittere
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Typography variant="h1">Mittere</Typography>
+            <Typography variant="body1">
+              Take control of your support pipeline so you can support the
+              world.
             </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "ltr" ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            <ListItem button>
-              <ListItemIcon>
-                <ContactsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Contacts" />
-            </ListItem>
-          </List>
-        </Drawer>
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open
-          })}
-        >
-          <div className={classes.drawerHeader} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
-        </main>
-      </div>
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute exact path="/home">
+            <Typography variant="h1">Welcome to Mittere!</Typography>
+          </PrivateRoute>
+          <PrivateRoute path="/contacts">
+            <ContactsPage />
+          </PrivateRoute>
+        </Switch>
+      </Router>
     </AuthProvider>
   );
 }
--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -4,6 +4,7 @@ import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/pipeable";
 
 import { AuthContext } from "./Auth";
+import AppLayout from "./AppLayout";
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
@@ -15,19 +16,21 @@ function PrivateRoute(props: any) {
   return (
     <Route
       {...rest}
-      render={({ location }) =>
-        pipe(
-          currentUser,
-          O.fold(
-            () => (
-              <Redirect
-                to={{ pathname: "/login", state: { from: location } }}
-              />
-            ),
-            () => children
-          )
-        )
-      }
+      render={({ location }) => (
+        <AppLayout>
+          {pipe(
+            currentUser,
+            O.fold(
+              () => (
+                <Redirect
+                  to={{ pathname: "/login", state: { from: location } }}
+                />
+              ),
+              () => children
+            )
+          )}
+        </AppLayout>
+      )}
     />
   );
 }
